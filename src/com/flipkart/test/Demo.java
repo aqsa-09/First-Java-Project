81777467646206536424bced65a52f17c232a052
aqsa.a <aqsa.a@fk1089-215480l.fkipl.flipkart.com>
2024-06-25 15:32:01 +0530

    first java prog

---
+++ src/com/flipkart/test/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.test;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("My first java program");
+
+	}
+
+}
